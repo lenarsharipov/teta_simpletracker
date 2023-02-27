e17e237cbb3024f0f9b120236082d3d02bf5220c
lenarsharipov <lenarsharipov@gmail.com>
2023-02-27 14:44:55 +0300

    upload Store interface

---
+++ src/main/java/tr/com/teta/tracker/Store.java
@@ -0,0 +1,31 @@
+package tr.com.teta.tracker;
+
+import tr.com.teta.tracker.model.Company;
+import tr.com.teta.tracker.model.Subscriber;
+
+import java.util.List;
+
+public interface Store {
+    Company addCompany(Company company);
+
+    boolean updateCompany(int id, Company company);
+
+    boolean deleteCompany(int id);
+
+    List<Company> findAllCompanies();
+
+    Subscriber addSubscriber(Subscriber subscriber);
+
+    boolean updateSubscriber(int id, Subscriber subscriber);
+
+    boolean deleteSubscriber(int id);
+
+    List<Company> findAllSubscribers();
+
+    Subscriber findByName(String name, String surname);
+
+    Subscriber findBySubscriberNumber(String subscriberNumber);
+
+    List<Subscriber> findByPlate(String plateNumber);
+
+}
